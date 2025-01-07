a30336b332ac71a62fccac9813b6590ff36c8fc3
asw5926 <asw5926@gap.nthu.edu.tw>
2025-01-07 22:34:38 +0800

    Auto commit from batch script

--- git upload.bat
+++ git upload.bat
@@ -1,6 +1,10 @@
 @echo off
 :: 移動到你的專案目錄
+<<<<<<< HEAD
 cd C:\Users\Home\Desktop\東東\python_jupyter notebook
+=======
+cd C:\Users\asw5926\Documents\me\stock
+>>>>>>> 40a47e4 (Auto commit from batch script)
 
 :: 顯示當前目錄
 echo Working in %cd%
